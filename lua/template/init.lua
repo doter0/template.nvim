e63aae6da1803479ec4d1436a76ab4ff68550ae9
doter0 <hollywooddreaming21@gmail.com>
2024-10-26 05:19:20 +0900

    Create tpl_insert

--- lua/template/init.lua
+++ lua/template/init.lua
@@ -1,7 +1,17 @@
-local tpl_manager= require('template.template_manager')
 
+local tpl_manager = require('template.template_manager')
 local command = vim.api.nvim_create_user_command
 
-command('TemplateOpen',function(opts) tpl_manager.tpl_open(opts.args) end, { nargs = 1})
-command('TemplateAdd',function(opts) tpl_manager.tpl_add(opts.args) end, { nargs = 1})
-command('TemplateDelate',function(opts) tpl_manager.tpl_del(opts.args) end, { nargs = 1})
+local M = {}
+
+command('TemplateOpen',function(opts) tpl_manager.tpl_open(opts.args) end, { nargs = 1 })
+command('TemplateAdd',function(opts) tpl_manager.tpl_add(opts.args) end, { nargs = 1 })
+command('TemplateDelete', function(opts) tpl_manager.tpl_del(opts.args) end, { nargs = 1 })
+command('TemplateInsert', function(opts) tpl_manager.tpl_insert(opts.args) end, { nargs = 1 })
+
+function M.setup()
+
+end
+
+return M
+
--- lua/template/template_manager.lua
+++ lua/template/template_manager.lua
@@ -1,35 +1,47 @@
-local tpl_dir = vim.fn.expand("~/templates/")
+local tpl_dir = vim.fn.expand("C:/Users/doter/templates/")
 local tpl_ext = ".tpl"
 
 local M = {}
 
-M.tpl_open = function(tpl_name)
-    local file_path = tpl_dir .. "/" .. tpl_name .. tpl_ext
+M.tpl_open = function(name)
+    local file_path = tpl_dir .. name .. tpl_ext
     if vim.fn.filereadable(file_path) == 1 then
         vim.cmd("vsplit " .. file_path)
     else
-        print("Template '" .. tpl_name .. "' not found.")
+        print("Template '" .. name .. "' not found.")
     end
 end
 
-M.tpl_add = function(tpl_name)
-    local file_path = tpl_dir .. "/" .. tpl_name .. tpl_ext
+M.tpl_add = function(name)
+    local file_path = tpl_dir .. name .. tpl_ext
     if vim.fn.filereadable(file_path) == 1 then
-        print("Template '" .. tpl_name .. "' already exists.")
+        print("Template '" .. name .. "' already exists.")
     else
-        vim.fn.writefile({}, file_path)
-        print("Template '" .. tpl_name .. "' created.")
+        vim.fn.mkdir(tpl_dir, "p")
+        print("Template '" .. name .. "' created.")
     end
     vim.cmd("vsplit " .. file_path)
 end
 
-M.tpl_del = function(tpl_name)
-    local file_path = tpl_dir .. "/" .. tpl_name .. tpl_ext
+M.tpl_del = function(name)
+    local file_path = tpl_dir .. name .. tpl_ext
     if vim.fn.filereadable(file_path) == 1 then
         vim.fn.delete(file_path)
-        print("Template '" .. tpl_name .. "' deleted.")
+        print("Template '" .. name .. "' deleted.")
     else
-        print("Template '" .. tpl_name .. "' not found.")
+        print("Template '" .. name .. "' not found.")
+    end
+end
+
+M.tpl_insert = function(name)
+    local file_path = tpl_dir .. name .. tpl_ext
+    if vim.fn.filereadable(file_path) == 1 then
+        local content = vim.fn.readfile(file_path)
+        local cursor_pos = vim.api.nvim_win_get_cursor(0)
+        vim.api.nvim_buf_set_lines(0, cursor_pos[1], cursor_pos[1], false, content)
+        vim.api.nvim_win_set_cursor(0, {cursor_pos[1] + #content, 0})
+    else
+        print("Template '" .. name .. "' not found.")
     end
 end
 
